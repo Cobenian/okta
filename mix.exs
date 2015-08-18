0b3359e5d53d4fd679f1da2abed63544db0cf4d2
Adam Guyot <adam@cobenian.com>
2015-08-18 13:39:06 -0400

    fixing for hex publication

--- mix.exs
+++ mix.exs
@@ -7,7 +7,10 @@ defmodule Okta.Mixfile do
      elixir: "~> 1.0",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
-     deps: deps]
+     deps: deps,
+     description: description,
+     source_url: "https://github.com/Cobenian/okta",
+     package: package ]
   end
 
   # Configuration for the OTP application
@@ -15,20 +18,26 @@ defmodule Okta.Mixfile do
   # Type `mix help compile.app` for more information
   def application do
     [applications: [:logger, :httpoison]]
-    #[applications: [:logger]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type `mix help deps` for more examples and options
   defp deps do
     [ {:httpoison, "~> 0.7.2"},
       {:poison, "~> 1.4.0"} ]
   end
+
+  defp description do
+    """
+    An Elixir Library for interfacing Okta
+    """
+  end
+
+
+  defp package do
+      [# These are the default files included in the package
+       files: ["lib", "files", "priv", "mix.exs", "README*", "readme*", "LICENSE*", "license*", "*LICENSE"],
+       contributors: ["Adam Guyot"],
+       licenses: ["Apache 2.0"],
+       links: %{"GitHub" => "https://github.com/Cobenian/okta"}]
+  end
 end
+
