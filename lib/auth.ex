b5460bf2a45afbbbf84e29c062fc4e8cde1efe86
Adam Guyot <adam@cobenian.com>
2015-08-18 21:18:49 -0400

    switch char lists to strings

--- lib/auth.ex
+++ lib/auth.ex
@@ -1,14 +1,12 @@
 defmodule Okta.Auth do
 
     def authn(okta, user, password) do
-      headers = [{'Authorization', "SSWS " <> okta[:api_token]},
-                 {'Accept', 'application/json'},
+      headers = [{"Authorization", "SSWS " <> okta[:api_token]},
+                 {"Accept", "application/json"},
                  {"Content-type", "application/json"}]
-
        {:ok, response} = HTTPoison.post(okta[:site] <> okta[:api_ver] <> "/authn",    
             "{\"username\": \"#{user}\", \"password\": \"#{password}\"}",
             headers)
-
        Poison.decode! response.body
     end
 end
